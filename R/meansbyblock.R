23d0138fc8c6fa7e233e4de7c5ef738b796af8dd
Corey Bohil <corey.bohil@ucf.edu>
2020-02-05 22:04:44 -0500

    add cataccuracy function

--- R/meansbyblock.R
+++ R/meansbyblock.R
@@ -4,3 +4,13 @@ meansByBlock = function(data) {
   meansOut <- aggregate(data, list(data$block), mean)
   return(meansOut)
 }
+
+# compute accuracy by block (any number of categories)
+catAccuracy = function(data) {
+  # data needs to be a list of data frames
+  # each data set needs columns labeled "block", "cat", & "resp"
+  warning("catAccuracy needs to be generalized to send in labels for block, cat & resp")
+  accuracy <- lapply(data, function(data) {lapply(split(data, data$block), function(data) {length(which(data$cat == data$resp))/nrow(data)})})
+  accuracy <- matrix( unlist(accuracy), nrow=length(accuracy) , byrow = TRUE)
+  return(accuracy)
+}
