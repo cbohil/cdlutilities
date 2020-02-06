1f9096f8e8f7792fe7208a3316053304acb4ad33
Corey Bohil <corey.bohil@ucf.edu>
2020-02-06 06:49:23 -0500

    added several utility functions in single file

--- R/CDL-utilities.R
+++ R/CDL-utilities.R
@@ -14,3 +14,13 @@ catAccuracy = function(data) {
   accuracy <- matrix( unlist(accuracy), nrow=length(accuracy) , byrow = TRUE)
   return(accuracy)
 }
+
+# summarize matrix values
+matrixSummary = function(data) {
+  columnMean <- colMeans(data)
+  columnSD <- apply(data, 2, sd)
+  columnSE <-columnSD / sqrt(nrow(data))
+  returnList <- list(columnMean, columnSD, columnSE)
+  names(returnList) <- c("colMeans", "colSD", "colSE")
+  return(returnList)
+}
