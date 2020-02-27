4e2ec4d11b9fa0bb02e533731545d8ade9037d5f
Corey Bohil <corey.bohil@ucf.edu>
2020-02-27 06:42:27 -0500

    removed warning from catAccuracy

--- R/cataccuracy.R
+++ R/cataccuracy.R
@@ -4,7 +4,6 @@
 catAccuracy = function(data) {
   # data needs to be a list of data frames
   # each data set needs columns labeled "block", "cat", & "resp"
-  warning("catAccuracy needs to be generalized to send in labels for block, cat & resp")
   accuracy <- lapply(data, function(data) {lapply(split(data, data$block), function(data) {length(which(data$cat == data$resp))/nrow(data)})})
   accuracy <- matrix( unlist(accuracy), nrow=length(accuracy) , byrow = TRUE)
   return(accuracy)
